da1c0fb6629f9e01752ee0eb7bed1fe0e2a4cfd5
lisaevermore <70776430+lisaevermore@users.noreply.github.com>
2023-01-18 01:07:47 +0000

    add some variables

---
+++ assets/js/logic.js
@@ -0,0 +1,4 @@
+// Variables
+let time = 60; // starting time in seconds
+let currentQuestion = 0; // keep track of current question
+let score = 0; // keep track of score
---
+++ assets/js/questions.js
@@ -0,0 +1,13 @@
+let question = [
+    {
+      title: 'Which of these expressions is NOT a valid way to add 1 to a variable in JavaScript?',
+      choice: ["X++", "X+", "X = X + 1", "X +=1"],
+      answer: 1
+    },
+      
+    {
+      title: 'What tag is used to define the bottom section (footer) of an HTML document?',
+      choice: ["<button>", "<h1> to <h6>", "<td>", "<footer>"],
+      answer: 3,
+    }
+  ];
