3137089ed8328e61317624a1c8e076cfe25db870
lisaevermore <70776430+lisaevermore@users.noreply.github.com>
2023-01-18 01:09:36 +0000

    select elements and added some event listeners

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,3 +2,19 @@
 let time = 60; // starting time in seconds
 let currentQuestion = 0; // keep track of current question
 let score = 0; // keep track of score
+// select elements
+const startButton = document.getElementById("start");
+const questionsDiv = document.getElementById("questions");
+const questionTitle = document.getElementById("question-title");
+const choicesDiv = document.getElementById("choices");
+const endScreen = document.getElementById("end-screen");
+const finalScore = document.getElementById("final-score");
+const initialsInput = document.getElementById("initials");
+const submitButton = document.getElementById("submit");
+const feedback = document.getElementById("feedback");
+const timer = document.getElementById("time");
+
+// Event listeners
+startButton.addEventListener("click", startQuiz);
+choicesDiv.addEventListener("click", checkAnswer);
+submitButton.addEventListener("click", saveScore);
